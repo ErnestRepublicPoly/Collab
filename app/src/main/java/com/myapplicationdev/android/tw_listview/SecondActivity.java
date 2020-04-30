7c53e1f5bba3fc298fb56e6fa483fc04ab30e8a1
18050268 <mikunation01>
2020-04-30 14:57:50 +0800

    Completion

--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -14,7 +14,7 @@ public class SecondActivity extends AppCompatActivity {
     ListView lv;
     TextView tvYear;
     ArrayList<Module> year1, year2, year3;
-    ArrayAdapter<String> aa;
+    ArrayAdapter aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +28,17 @@ public class SecondActivity extends AppCompatActivity {
         String year = i.getStringExtra("year");
         tvYear.setText(year);
 
-        year1 = new ArrayList<>();
+        year1 = new ArrayList<Module>();
         year1.add(new Module("C208", R.drawable.prog));
         year1.add(new Module("C200", R.drawable.nonprog));
         year1.add(new Module("C346", R.drawable.prog));
 
-        year2 = new ArrayList<>();
+        year2 = new ArrayList<Module>();
         year2.add(new Module("C207", R.drawable.prog));
         year2.add(new Module("C273", R.drawable.prog));
         year2.add(new Module("B216", R.drawable.nonprog));
 
-        year3 = new ArrayList<>();
+        year3 = new ArrayList<Module>();
         year3.add(new Module("C349", R.drawable.prog));
         year3.add(new Module("C302", R.drawable.prog));
         year3.add(new Module("C347", R.drawable.prog));
