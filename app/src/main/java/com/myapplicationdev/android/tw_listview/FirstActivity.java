212f9ccaf502ab0354badbb52b3e921892672b4b
18016517 <18016517@myrp.edu.sg>
2020-04-30 14:40:21 +0800

    ddd

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,7 +1,10 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -30,8 +33,15 @@ public class FirstActivity extends AppCompatActivity {
 
         lv.setAdapter(arrayAdapter);
 
-
-
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent;
+                intent = new Intent(getBaseContext(), SecondActivity.class);
+                intent.putExtra("selectedType", al.get(i));
+                startActivity(intent);
+            }
+        });
 
     }
 }
